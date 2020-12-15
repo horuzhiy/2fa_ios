63573b5ef1fe08fa6ec4310f04bdd3b338eb301f
Alexander Horuzhiy <horuzhiy@gmail.com>
2020-12-15 20:19:45 +0300

    Initial Commit

---
+++ 2fa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2fa
+//
+//  Created by Alexander on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
